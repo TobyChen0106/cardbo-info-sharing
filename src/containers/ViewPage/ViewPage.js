eeae47a2e77fd8af3f90929d5bc15fb6d12cb8d5
TobyChen0106 <awesdjzo@gmail.com>
2020-06-26 19:16:16 +0800

    update

--- src/containers/ViewPage/ViewPage.js
+++ src/containers/ViewPage/ViewPage.js
@@ -176,7 +176,7 @@ class ViewPage extends Component {
                         userImage: profile.pictureUrl,
                     }
                 });
-                console.log(profile.pictureUrl)
+                window.alert(profile.pictureUrl)
             }
         }).then(() => {
             this.setState({
